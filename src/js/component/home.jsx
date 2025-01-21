1f4c07c66e23aba365e27629c7f9938b78bd7eae
kazou92 <99810202+kazou92@users.noreply.github.com>
2025-01-21 04:10:26 +0000

    parte removido ya que iba hacer esta parte de arriba del semaforo pero al final lo deje asi

--- src/js/component/home.jsx
+++ src/js/component/home.jsx
@@ -9,7 +9,7 @@ const Home = () => {
 	
 	return (
 		<div className="text-center bg-dark  mx-4 d-flex flex-column justify-content-end" style={{width:"200px",height:"500px",marginTop:"150px",borderRadius:"15px",position:""}} >
-	<div className="order border-1 border-white"  style={{width:"100px",height:"100px",position:""}}> </div>
+	
 
 	        <Lamp initialColor="bg-danger" />
             <Lamp initialColor="bg-warning" />
